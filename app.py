1f45f36f5ce43d48e4ec8076004c7fdebd8f4df9
srianshurath07 <sriyanshurath07@gmail.com>
2025-08-30 10:00:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ if page == "Dashboard":
                 <div class="card">
                     <div class="metric-label">{metric['label']}</div>
                     <div class="metric-value">{metric['value']}</div>
-                    <div style="font-size:12px; color:gray;">{metric['desc']}</div>
+                    <div style="font-size:12px; color:black;">{metric['desc']}</div>
                 </div>
                 """,
                 unsafe_allow_html=True
@@ -43,8 +43,8 @@ elif page == "Profile":
         if user_id:
             try:
                 response = requests.get(
-                    "https://your-backend.com/recommend",  # Replace with your actual FastAPI URL
-                    params={"user_id": user_id}
+                    "http://127.0.0.1:8000/recommend",  # Replace with your actual FastAPI URL
+                    params={"user_id": 101}
                 )
                 if response.status_code == 200:
                     data = response.json()
@@ -87,10 +87,12 @@ st.markdown(
     .metric-label {
         font-weight: bold;
         font-size: 16px;
+        font-color: #000000
     }
     .metric-value {
         font-size: 24px;
         margin-top: 0.5rem;
+        font-color: #000000
     }
     </style>
     """,
